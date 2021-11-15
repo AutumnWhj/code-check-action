63b356ad4059e616bc0d5c32434c695de43ba8f6
Autumn <439661734@qq.com>
2021-11-15 15:42:17 +0800

    chore: test3

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,25 @@
 // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+import {debug, getInput, setFailed} from '@actions/core'
 import {parseInputArray, runEslint} from './check/eslint'
-import core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    const githubToken: string = core.getInput('githubToken', {
+    const githubToken: string = getInput('githubToken', {
       required: true
     })
-    const eslintFiles: string = core.getInput('eslintFiles') || '.' // all files by default
-    const eslintConfig: string = core.getInput('eslintConfig') || '.eslintrc' // .eslintrc by default
-    const eslintExtensions: string =
-      core.getInput('eslintExt') || 'js, ts, jsx, tsx'
+    const eslintFiles: string = getInput('eslintFiles') || '.' // all files by default
+    const eslintConfig: string = getInput('eslintConfig') || '.eslintrc' // .eslintrc by default
+    const eslintExtensions: string = getInput('eslintExt') || 'js, ts, jsx, tsx'
     const inputOptions = {
       eslintFiles: parseInputArray(eslintFiles),
       eslintConfig,
       eslintExtensions: parseInputArray(eslintExtensions)
     }
-    core.debug(`githubToken:${githubToken}`)
-    core.debug(`options:${inputOptions}`)
+    debug(`githubToken:${githubToken}`)
+    debug(`options:${inputOptions}`)
     runEslint(githubToken, inputOptions)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) setFailed(error.message)
   }
 }
 
