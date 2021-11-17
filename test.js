42a8dbb08c381d092274e0736853a10021b22607
Autumn <439661734@qq.com>
2021-11-17 10:52:56 +0800

    chore: test eslint12

---
+++ test.js
@@ -0,0 +1,25 @@
+const {ESLint} = require('eslint')
+const path = require('path')
+const repoPath = '/Users/Autumn/Documents/Autumn/github/code-check-action'
+const pathRelative= (location) =>{
+  return path.resolve(repoPath, location)
+}
+const cliOptions = {
+  useEslintrc: false,
+  overrideConfigFile: pathRelative('./.eslintrc.json'),
+  extensions: [ 'ts' ],
+  cwd: pathRelative('./'),
+}
+console.log('cliOptions: ', cliOptions);
+const eslint = new ESLint(cliOptions)
+
+const eslintFiles = ['./src']
+const lintFiles = eslintFiles.map(location =>
+
+  path.resolve(repoPath, location)
+)
+
+console.log('lintFiles: ', lintFiles);
+eslint.lintFiles(lintFiles).then((res) => {
+  console.log('lintFiles', res)
+})
